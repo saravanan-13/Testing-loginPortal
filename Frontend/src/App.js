dac32590f65e47eed6f11f178a4aeae63786dec1
Saravanan Rajendran <sarrajen@publicisgroupe.net>
2019-08-13 22:13:36 +0530

    ALPHA Code Added

---
+++ Frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Main from './components/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Home</h1>
+      <br/>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
